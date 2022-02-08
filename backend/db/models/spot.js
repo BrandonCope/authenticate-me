cd6933ed825d8c183351347f625eada70cee35ed
brandon <bcopeland97@gmail.com>
2022-02-07 20:00:30 -0500

    end of day

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -13,6 +13,8 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   Spot.associate = function(models) {
     // associations can be defined here
+    Spot.belongsTo(models.User, {foreignKey: 'userId'});
+    Spot.hasMany(models.Image, {foreignKey: 'spotId'})
   };
   return Spot;
 };
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 
-const {Spots} = require('../../db/models')
+const router = express.Router();
+
+const {Spot} = require('../../db/models')
 
 
+router.get('', asyncHandler(async (req, res) => {
+    const spots = await Spot.findAll();
+    console.log(spots)
+    res.json(spots)
+}))
+
+module.exports = router;
