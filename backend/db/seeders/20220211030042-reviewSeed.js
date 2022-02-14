02d39337551215d36e2c6f74ed14d43f95664991
brandon <bcopeland97@gmail.com>
2022-02-13 20:06:05 -0500

    seed4

--- backend/db/seeders/20220211030042-reviewSeed.js
+++ backend/db/seeders/20220211030042-reviewSeed.js
@@ -10,7 +10,7 @@ module.exports = {
       */
    return queryInterface.bulkInsert('Reviews', [
      {
-       userId:3,
+       userId:1,
        spotId:1,
        username:"Demo-User",
        review:"Loved the fountain.",
